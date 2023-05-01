394d6d3df7d20357795db9b59c3c9ffa24296d45
Rafael Huang <101841917+Rafael-Huang@users.noreply.github.com>
2023-05-01 16:25:17 -0400

    Update HangManFunctions.h

--- HangManFunctions.h
+++ HangManFunctions.h
@@ -4,11 +4,27 @@
 #include <iostream>
 #include <vector>
 
-//Here are all the functions declared.
-void DisplayUserMisses(int missedLetters);
-
-void DisplayStatus(std::vector<char> incorrect, std::string userAnswer);
-
-void EndGame(std::string userAnswer, std::string codeword);
-
+class HangMan {
+public:
+    std::string codeword;
+    std::string userAnswer;
+    std::vector<char> incorrectGuesses;
+    int missedLetters;
+    
+    //Below are all the functions declared.
+    
+    // Function will explain how the Hang Man game works.
+    void DisplayInstructions();
+    
+    // Function does many conditions if the user misses, which means everytime the user misses a letter, the image will change.
+    void DisplayUserMisses(int);
+    
+    // Function will display user status, like what words did he used and were incorrect.
+    void DisplayStatus(std::vector<char>, std::string);
+    
+    // Function will display final message if the user get the correct or incorrect.
+    void EndGame(std::string, std::string);
+    
+};
 #endif // HangMan_Functions_h
+
